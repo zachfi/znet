1273cfa891fcd2ccfc358fdab532135cc5c88bf2
Zach Leslie <xaque208@gmail.com>
2018-12-23 21:31:37 -0800

    Flesh out more of the configuration

--- znet/config.go
+++ znet/config.go
@@ -1,18 +1,41 @@
 package znet
 
 import (
+	"errors"
+	"fmt"
 	"io/ioutil"
 	"path/filepath"
 
-	"errors"
-	"fmt"
 	log "github.com/sirupsen/logrus"
 	"gopkg.in/yaml.v2"
 )
 
 type Config struct {
-	Rooms    []Room `yaml:"rooms"`
-	Endpoint string `yaml:"endpoint"`
+	Rooms    []Room      `yaml:"rooms"`
+	Endpoint string      `yaml:"endpoint"`
+	nats     NatsConfig  `yaml:"nats,omitempty"`
+	junos    NatsConfig  `yaml:"junos,omitempty"`
+	redis    RedisConfig `yaml:"redis,omitempty"`
+	http     HttpConfig  `yaml:"http,omitempty"`
+}
+
+type NatsConfig struct {
+	URL   string
+	Topic string
+}
+
+type RedisConfig struct {
+	Host string
+}
+
+type JunosConfig struct {
+	Hosts      []string
+	Username   string
+	PrivateKey string
+}
+
+type HttpConfig struct {
+	ListenAddress string
 }
 
 type Room struct {
