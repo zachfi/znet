6292175ad536d031eb7f3b9057a647f0d78f1c9c
Zach Leslie <xaque208@gmail.com>
2019-01-13 15:04:20 -0800

    Include BGP structs

--- znet/data.go
+++ znet/data.go
@@ -8,5 +8,37 @@ type Data struct {
 }
 
 type HostData struct {
-	NTPServers []string `yaml:"ntp_servers"`
+	NTPServers            []string              `yaml:"ntp_servers" json:"ntp_servers"`
+	DHCPServer            string                `yaml:"dhcp_server"`
+	DHCPForwardInterfaces []string              `yaml:"dhcp_forward_interfaces"`
+	LLDPInterfaces        []string              `yaml:"lldp_interfaces"`
+	IRBInterfaces         []IRBInterface        `yaml:"irb_interfaces"`
+	RouterAdvertisements  []RouterAdvertisement `yaml:"router_advertisements"`
+	BGP                   BGP                   `yaml:"bgp"`
+}
+
+type BGP struct {
+	Groups []BGPGroup `yaml:"groups"`
+}
+
+type BGPGroup struct {
+	Name      string   `yaml:"name"`
+	Type      string   `yaml:"type"`
+	ASN       int      `yaml:"asn"`
+	Neighbors []string `yaml:"neighbors"`
+	Import    []string `yaml:"import"`
+	Export    []string `yaml:"export"`
+}
+
+type RouterAdvertisement struct {
+	Interface string `yaml:"interface"`
+	DNSServer string `yaml:"dns_server"`
+	Prefix    string `yaml:"prefix"`
+}
+
+type IRBInterface struct {
+	Unit  string   `yaml:"unit"`
+	Inet  []string `yaml:"inet"`
+	Inet6 []string `yaml:"inet6"`
+	MTU   int      `yaml:"mtu"`
 }
