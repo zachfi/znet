80143a6a9f744e1b97ff65e9e530104801922a3d
Zach Leslie <xaque208@gmail.com>
2019-12-01 11:13:41 -0800

    Comment what does not work

--- znet/environment.go
+++ znet/environment.go
@@ -2,43 +2,41 @@ package znet
 
 import (
 	"fmt"
-
-	log "github.com/sirupsen/logrus"
 )
 
-func (z *Znet) LoadEnvironment() error {
+// LoadEnvironment reads reads environment variables out of vault for return.
+func LoadEnvironment(config VaultConfig, env string) (map[string]string, error) {
 
 	environment := make(map[string]string)
-	if z.Config.Vault.Host == "" || z.Config.Vault.VaultPath == "" {
-		return fmt.Errorf("Incomplete vault configuration, unable to load Environment")
+	if config.Host == "" || config.VaultPath == "" {
+		return map[string]string{}, fmt.Errorf("Incomplete vault configuration, unable to load Environment")
 	}
 
-	s, err := z.NewSecretClient(z.Config.Vault)
-	if err != nil {
-		return err
-	}
+	// s, err := NewSecretClient(config)
+	// if err != nil {
+	// 	return map[string]string{}, err
+	// }
 
-	for _, e := range z.Config.Environments {
-		if e.Name == "default" {
+	// TODO determine supported environments and their paths.
+	// for _, e := range z.Config.Environments {
+	// 	if e.Name == "default" {
+	//
+	// 		for _, k := range e.SecretValues {
+	// 			path := fmt.Sprintf("%s/%s", z.Config.Vault.VaultPath, k)
+	// 			log.Debugf("Reading vault path: %s", path)
+	// 			secret, err := s.Logical().Read(path)
+	// 			if err != nil {
+	// 				log.Error(err)
+	// 			}
+	//
+	// 			if secret != nil {
+	// 				environment[k] = secret.Data["value"].(string)
+	// 			}
+	//
+	// 		}
+	//
+	// 	}
+	// }
 
-			for _, k := range e.SecretValues {
-				path := fmt.Sprintf("%s/%s", z.Config.Vault.VaultPath, k)
-				log.Debugf("Reading vault path: %s", path)
-				secret, err := s.Logical().Read(path)
-				if err != nil {
-					log.Error(err)
-				}
-
-				if secret != nil {
-					environment[k] = secret.Data["value"].(string)
-				}
-
-			}
-
-		}
-	}
-
-	z.Environment = environment
-
-	return nil
+	return environment, nil
 }
