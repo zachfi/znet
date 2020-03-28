90ad94c2c876231119a0c913231ba5715682470d
Zach Leslie <xaque208@gmail.com>
2020-03-27 20:37:04 -0700

    Update build

---
+++ build/compile.mk
@@ -0,0 +1,61 @@
+#
+# Makefile Fragment for Compiling
+#
+
+GO         ?= go
+BUILD_DIR  ?= ./bin/
+PROJECT_MODULE ?= $(shell $(GO) list -m)
+# $b replaced by the binary name in the compile loop, -s/w remove debug symbols
+LDFLAGS    ?= "-s -w -X main.Version=$(PROJECT_VER) -X main.appName=$$b"
+SRCDIR     ?= .
+COMPILE_OS ?= freebsd linux
+
+# Determine commands by looking into cmd/*
+COMMANDS   ?= $(wildcard ${SRCDIR}/cmd/*)
+
+# Determine binary names by stripping out the dir names
+BINS       := $(foreach cmd,${COMMANDS},$(notdir ${cmd}))
+
+
+compile-clean:
+	@echo "=== $(PROJECT_NAME) === [ compile-clean    ]: removing binaries..."
+	@rm -rfv $(BUILD_DIR)/*
+
+compile: deps compile-only proto
+
+proto:
+	@echo "=== $(PROJECT_NAME) === [ proto compile    ]: compiling protobufs:"
+	protoc -I rpc/ rpc/rpc.proto --go_out=plugins=grpc:rpc
+
+compile-all: deps-only
+	@echo "=== $(PROJECT_NAME) === [ compile          ]: building commands:"
+	@mkdir -p $(BUILD_DIR)/$(GOOS)
+	@for b in $(BINS); do \
+		for os in $(COMPILE_OS); do \
+			echo "=== $(PROJECT_NAME) === [ compile          ]:     $(BUILD_DIR)$$os/$$b"; \
+			BUILD_FILES=`find $(SRCDIR)/cmd/$$b -type f -name "*.go"` ; \
+			GOOS=$$os $(GO) build -ldflags=$(LDFLAGS) -o $(BUILD_DIR)/$$os/$$b $$BUILD_FILES ; \
+		done \
+	done
+
+compile-only: deps-only
+	@echo "=== $(PROJECT_NAME) === [ compile          ]: building commands:"
+	@mkdir -p $(BUILD_DIR)/$(GOOS)
+	@for b in $(BINS); do \
+		echo "=== $(PROJECT_NAME) === [ compile          ]:     $(BUILD_DIR)$(GOOS)/$$b"; \
+		BUILD_FILES=`find $(SRCDIR)/cmd/$$b -type f -name "*.go"` ; \
+		GOOS=$(GOOS) $(GO) build -ldflags=$(LDFLAGS) -o $(BUILD_DIR)/$(GOOS)/$$b $$BUILD_FILES ; \
+	done
+
+# Override GOOS for these specific targets
+compile-darwin: GOOS=darwin
+compile-darwin: deps-only compile-only
+
+compile-linux: GOOS=linux
+compile-linux: deps-only compile-only
+
+compile-windows: GOOS=windows
+compile-windows: deps-only compile-only
+
+
+.PHONY: clean-compile compile compile-darwin compile-linux compile-only compile-windows
---
+++ build/release.mk
@@ -0,0 +1,22 @@
+RELEASE_SCRIPT ?= ./scripts/release.sh
+
+GOTOOLS += github.com/goreleaser/goreleaser
+
+REL_CMD ?= goreleaser
+DIST_DIR ?= ./dist
+
+ARCHIVE_URL       ?= https://github.com/newrelic/$(strip $(PROJECT_NAME))/archive/v$(strip $(PROJECT_VER_TAGGED)).tar.gz
+
+# Example usage: make release version=0.11.0
+release: build
+	@echo "=== $(PROJECT_NAME) === [ release          ]: Generating release."
+	$(RELEASE_SCRIPT) $(version)
+
+release-clean:
+	@echo "=== $(PROJECT_NAME) === [ release-clean    ]: distribution files..."
+	@rm -rfv $(DIST_DIR)/*
+
+release-publish: clean tools docker-login
+	@echo "=== $(PROJECT_NAME) === [ release-publish  ]: Publishing release via $(REL_CMD)"
+	$(REL_CMD)
+
