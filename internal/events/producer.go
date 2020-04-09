ab287c7cd90671aa82c3b075f79677fd794bad1c
Zach Leslie <xaque208@gmail.com>
2020-04-09 10:08:52 -0700

    chore(tidy): clean up old ideas

--- internal/events/producer.go
+++ internal/events/producer.go
@@ -4,8 +4,6 @@ package events
 type Producer interface {
 	// Produce is used to pass the
 	Produce(interface{}) error
-	// EpochCollection()
 	Start() error
 	Stop() error
-	// scheduler() error
 }
