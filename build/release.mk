3d5e54c609639211df1aaa959f0631c097d1ccf9
Zach Leslie <xaque208@gmail.com>
2020-04-25 22:30:52 -0700

    fix(release): update release process

--- build/release.mk
+++ build/release.mk
@@ -1,5 +1,3 @@
-RELEASE_SCRIPT ?= ./scripts/release.sh
-
 GOTOOLS += github.com/goreleaser/goreleaser
 
 REL_CMD ?= goreleaser
@@ -8,15 +6,13 @@ DIST_DIR ?= ./dist
 ARCHIVE_URL       ?= https://github.com/xaque208/$(strip $(PROJECT_NAME))/archive/v$(strip $(PROJECT_VER_TAGGED)).tar.gz
 
 # Example usage: make release version=0.11.0
-release: build
-	@echo "=== $(PROJECT_NAME) === [ release          ]: Generating release."
-	$(RELEASE_SCRIPT) $(version)
+release: build release-publish
 
 release-clean:
 	@echo "=== $(PROJECT_NAME) === [ release-clean    ]: distribution files..."
 	@rm -rfv $(DIST_DIR)/*
 
-release-publish: clean tools docker-login
+release-publish: clean tools
 	@echo "=== $(PROJECT_NAME) === [ release-publish  ]: publishing release via $(REL_CMD)"
 	$(REL_CMD)
 
