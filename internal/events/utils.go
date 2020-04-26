42439df9b409d4f667528a5bda1b5f6fd7f4db07
Zach Leslie <xaque208@gmail.com>
2020-04-26 15:49:24 -0700

    chore(znet): include common events producer

--- internal/events/utils.go
+++ internal/events/utils.go
@@ -1,10 +1,15 @@
 package events
 
 import (
+	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
 	"reflect"
+	"time"
 
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 
 	pb "github.com/xaque208/znet/rpc"
 )
@@ -21,4 +26,38 @@ func MakeEvent(t interface{}) *pb.Event {
 	}
 
 	return req
+}
+
+func ProduceEvent(conn *grpc.ClientConn, ev interface{}) error {
+	// Create the RPC client
+
+	if conn == nil {
+		return fmt.Errorf("unable to make use of nil grpc client")
+	}
+
+	ec := pb.NewEventsClient(conn)
+	// t := reflect.TypeOf(ev).String()
+
+	var req *pb.Event
+
+	req = MakeEvent(ev)
+
+	if req == nil {
+		return fmt.Errorf("failed to make event")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	log.Tracef("producing RPC event %+v", req)
+	res, err := ec.NoticeEvent(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	if res.Errors {
+		return errors.New(res.Message)
+	}
+
+	return nil
 }
