c3b73cd5eb8ef432eb3c1e742229ad61d9f50764
Zach Leslie <xaque208@gmail.com>
2020-05-11 10:35:09 -0700

    feat(ci): include semver parsing for latest tag retrieval

--- pkg/continuous/ci.go
+++ pkg/continuous/ci.go
@@ -1,6 +1,10 @@
 package continuous
 
 import (
+	"fmt"
+	"strings"
+
+	"github.com/blang/semver"
 	git "github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
 	log "github.com/sirupsen/logrus"
@@ -20,6 +24,56 @@ func NewCI(url, cacheDir, sshKeyPath string) *CI {
 	}
 
 	return c
+}
+
+func (c *CI) LatestTag() string {
+	err := CacheRepo(c.URL, c.CacheDir, c.SSHKeyPath)
+	if err != nil {
+		log.Errorf("error while caching repo %s: %s", c.URL, err)
+	}
+
+	r, err := git.PlainOpen(c.CacheDir)
+	if err != nil {
+		log.Error(err)
+	}
+
+	_, tags, err := RepoRefs(r)
+	if err != nil {
+		log.Error(err)
+	}
+
+	var lastTag string
+	var prefix string
+
+	for n, _ := range tags {
+		if lastTag == "" {
+			lastTag = n
+		}
+
+		v, err := semver.ParseTolerant(n)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		nv, err := semver.ParseTolerant(lastTag)
+		if err != nil {
+			log.Error(err)
+			continue
+		}
+
+		if v.GT(nv) {
+			lastTag = v.String()
+
+			if strings.HasPrefix(n, "v") {
+				prefix = "v"
+			} else {
+				prefix = ""
+			}
+		}
+	}
+
+	return fmt.Sprintf("%s%s", prefix, lastTag)
 }
 
 func (c *CI) Fetch() (map[string]string, []string, error) {
