a081ffee4d6e2ba40818108f0335547f1b3bdde1
Zach Leslie <xaque208@gmail.com>
2020-08-23 10:53:59 -0700

    fix(lint): disperse complexity

--- znet/rpc_inventoryserver.go
+++ znet/rpc_inventoryserver.go
@@ -7,6 +7,7 @@ import (
 	log "github.com/sirupsen/logrus"
 
 	"github.com/xaque208/znet/internal/inventory"
+	"github.com/xaque208/znet/rpc"
 	pb "github.com/xaque208/znet/rpc"
 )
 
@@ -23,43 +24,9 @@ func (r *inventoryServer) Search(ctx context.Context, request *pb.SearchRequest)
 		return response, err
 	}
 
-	if hosts != nil {
-		for _, h := range *hosts {
-
-			host := &pb.NetworkHost{
-				Description:     h.Description,
-				Dn:              h.Dn,
-				Domain:          h.Domain,
-				Group:           h.Group,
-				Name:            h.Name,
-				OperatingSystem: h.OperatingSystem,
-				Platform:        h.Platform,
-				Role:            h.Role,
-				Type:            h.Type,
-			}
-
-			if h.Inet6Address != nil {
-				host.Inet6Address = *h.Inet6Address
-			}
-
-			if h.InetAddress != nil {
-				host.InetAddress = *h.InetAddress
-			}
-
-			if h.MacAddress != nil {
-				host.MacAddress = *h.MacAddress
-			}
-
-			if h.LastSeen != nil {
-				lastSeen, convError := ptypes.TimestampProto(*h.LastSeen)
-				if convError != nil {
-					log.Error(convError)
-				}
-				host.LastSeen = lastSeen
-			}
-
-			response.Hosts = append(response.Hosts, host)
-		}
+	pbHosts := getRPCHosts(hosts)
+	if pbHosts != nil {
+		response.Hosts = pbHosts
 	}
 
 	networkIDs, err := r.inventory.ListNetworkIDs()
@@ -67,31 +34,19 @@ func (r *inventoryServer) Search(ctx context.Context, request *pb.SearchRequest)
 		return response, err
 	}
 
-	if networkIDs != nil {
-		for _, h := range *networkIDs {
-			host := &pb.NetworkID{
-				Dn:   h.Dn,
-				Name: h.Name,
-			}
+	pbNetworkIDs := getRPCNetworkIDs(networkIDs)
+	if pbNetworkIDs != nil {
+		response.NetworkIds = pbNetworkIDs
+	}
 
-			if h.IpAddress != nil {
-				host.IpAddress = *h.IpAddress
-			}
+	zigbeeDevices, err := r.inventory.ListZigbeeDevices()
+	if err != nil {
+		return response, err
+	}
 
-			if h.MacAddress != nil {
-				host.MacAddress = *h.MacAddress
-			}
-
-			if h.ReportingSource != nil {
-				host.ReportingSource = *h.ReportingSource
-			}
-
-			if h.ReportingSourceInterface != nil {
-				host.ReportingSourceInterface = *h.ReportingSourceInterface
-			}
-
-			response.NetworkIds = append(response.NetworkIds, host)
-		}
+	pbZigbeeDevices := getRPCZigbeeDevices(zigbeeDevices)
+	if pbZigbeeDevices != nil {
+		response.ZigbeeDevices = pbZigbeeDevices
 	}
 
 	return response, nil
@@ -147,4 +102,117 @@ func (r *inventoryServer) ListNetworkHosts(ctx context.Context, request *pb.Empt
 	}
 
 	return response, nil
+}
+
+func getRPCHosts(hosts *[]inventory.NetworkHost) []*rpc.NetworkHost {
+	var response []*rpc.NetworkHost
+
+	if hosts != nil {
+		for _, h := range *hosts {
+
+			host := &pb.NetworkHost{
+				Description:     h.Description,
+				Dn:              h.Dn,
+				Domain:          h.Domain,
+				Group:           h.Group,
+				Name:            h.Name,
+				OperatingSystem: h.OperatingSystem,
+				Platform:        h.Platform,
+				Role:            h.Role,
+				Type:            h.Type,
+			}
+
+			if h.Inet6Address != nil {
+				host.Inet6Address = *h.Inet6Address
+			}
+
+			if h.InetAddress != nil {
+				host.InetAddress = *h.InetAddress
+			}
+
+			if h.MacAddress != nil {
+				host.MacAddress = *h.MacAddress
+			}
+
+			if h.LastSeen != nil {
+				lastSeen, convError := ptypes.TimestampProto(*h.LastSeen)
+				if convError != nil {
+					log.Error(convError)
+				}
+				host.LastSeen = lastSeen
+			}
+
+			response = append(response, host)
+		}
+	}
+
+	return response
+}
+
+func getRPCNetworkIDs(networkIDs *[]inventory.NetworkID) []*rpc.NetworkID {
+	var response []*rpc.NetworkID
+
+	if networkIDs != nil {
+		for _, h := range *networkIDs {
+			host := &pb.NetworkID{
+				Dn:   h.Dn,
+				Name: h.Name,
+			}
+
+			if h.IpAddress != nil {
+				host.IpAddress = *h.IpAddress
+			}
+
+			if h.MacAddress != nil {
+				host.MacAddress = *h.MacAddress
+			}
+
+			if h.ReportingSource != nil {
+				host.ReportingSource = *h.ReportingSource
+			}
+
+			if h.ReportingSourceInterface != nil {
+				host.ReportingSourceInterface = *h.ReportingSourceInterface
+			}
+
+			response = append(response, host)
+		}
+	}
+
+	return response
+}
+
+func getRPCZigbeeDevices(zigbeeDevices *[]inventory.ZigbeeDevice) []*rpc.ZigbeeDevice {
+	var response []*rpc.ZigbeeDevice
+
+	if zigbeeDevices != nil {
+		for _, h := range *zigbeeDevices {
+			zd := &pb.ZigbeeDevice{
+				Dn:               h.Dn,
+				Name:             h.Name,
+				Description:      h.Description,
+				IotZone:          h.IotZone,
+				Type:             h.Type,
+				SoftwareBuildId:  h.SoftwareBuildId,
+				DateCode:         h.DateCode,
+				Model:            h.Model,
+				Vendor:           h.Vendor,
+				ManufacturerName: h.ManufacturerName,
+				PowerSource:      h.PowerSource,
+				ModelId:          h.ModelId,
+			}
+
+			if h.LastSeen != nil {
+				lastSeen, convError := ptypes.TimestampProto(*h.LastSeen)
+				if convError != nil {
+					log.Error(convError)
+				}
+				zd.LastSeen = lastSeen
+			}
+
+			response = append(response, zd)
+		}
+	}
+
+	return response
 }
