0fca1b356cc211ced089ca88ff807d88d4d8e9cc
Zach Leslie <xaque208@gmail.com>
2020-09-19 15:55:35 -0700

    chore(astro): include event filter implementation

--- internal/astro/events.go
+++ internal/astro/events.go
@@ -1,10 +1,20 @@
 package astro
 
 import (
+	"encoding/json"
 	"time"
+
+	log "github.com/sirupsen/logrus"
+	"github.com/xaque208/znet/pkg/events"
 )
 
-// EventNames are the names of the events that this package will produce.
+// SolarEventNames are the names of the events that this package will produce.
+var SolarEventNames = []string{
+	"Sunrise",
+	"Sunset",
+	"PreSunset",
+}
+
 var EventNames = []string{
 	"SolarEvent",
 }
@@ -13,4 +23,25 @@ var EventNames = []string{
 type SolarEvent struct {
 	Name string
 	Time *time.Time
+}
+
+type SolarFilter struct {
+	Name []string
+}
+
+func (s *SolarFilter) Filter(ev events.Event) bool {
+	var solarEvent SolarEvent
+	err := json.Unmarshal(ev.Payload, &solarEvent)
+	if err != nil {
+		log.Error(err)
+		return false
+	}
+
+	for _, n := range s.Name {
+		if solarEvent.Name == n {
+			return true
+		}
+	}
+
+	return false
 }
