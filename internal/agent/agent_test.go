c57c07e6539e02e056123a77f7cbab4730756a2c
Zach Leslie <xaque208@gmail.com>
2020-09-19 16:01:31 -0700

    tests(agent): back out filter tests

--- internal/agent/agent_test.go
+++ internal/agent/agent_test.go
@@ -2,71 +2,12 @@ package agent
 
 import (
 	"testing"
-	"time"
 
-	"github.com/tj/assert"
-
-	"github.com/xaque208/znet/internal/gitwatch"
+	"github.com/stretchr/testify/require"
 )
 
-func TestPassFilter_Basic(t *testing.T) {
+func TestAgent_Basic(t *testing.T) {
 	config := Config{}
-
-	filter := Filter{
-		Names: []string{
-			"znet",
-		},
-		URLs:     []string{},
-		Branches: []string{},
-	}
-
-	ti := time.Now()
-
-	x := gitwatch.NewTag{
-		Time: &ti,
-		Name: "znet",
-		URL:  "https://github.com/xaque208/znet.git",
-		Tag:  "v0.16.3",
-	}
-
 	ag := NewAgent(config, nil)
-
-	assert.True(t, ag.passFilter(filter, x))
-}
-
-func TestPassFilter_Collection(t *testing.T) {
-	config := Config{}
-
-	filter := Filter{
-		Names:    []string{},
-		URLs:     []string{},
-		Branches: []string{},
-		Collections: []string{
-			"go_project",
-		},
-	}
-
-	ti := time.Now()
-
-	x := gitwatch.NewTag{
-		Time:       &ti,
-		Name:       "znet",
-		URL:        "https://github.com/xaque208/znet.git",
-		Tag:        "v0.16.3",
-		Collection: "go_project",
-	}
-
-	ag := NewAgent(config, nil)
-
-	assert.True(t, ag.passFilter(filter, x))
-
-	y := gitwatch.NewTag{
-		Time:       &ti,
-		Name:       "znet",
-		URL:        "https://github.com/xaque208/znet.git",
-		Tag:        "v0.16.3",
-		Collection: "go_projects",
-	}
-
-	assert.False(t, ag.passFilter(filter, y))
+	require.NotNil(t, ag)
 }
