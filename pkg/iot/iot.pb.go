ae55e7f00fb3b4effa1aff7cd81d19f2021c6e77
Zach Leslie <xaque208@gmail.com>
2021-01-18 21:31:22 -0800

    chore(iot): update after refactor

--- pkg/iot/iot.pb.go
+++ pkg/iot/iot.pb.go
@@ -2,16 +2,12 @@
 // versions:
 // 	protoc-gen-go v1.25.0
 // 	protoc        v3.12.4
-// source: iot.proto
+// source: pkg/iot/iot.proto
 
 package iot
 
 import (
-	context "context"
 	proto "github.com/golang/protobuf/proto"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -38,7 +34,7 @@ type Empty struct {
 func (x *Empty) Reset() {
 	*x = Empty{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_iot_proto_msgTypes[0]
+		mi := &file_pkg_iot_iot_proto_msgTypes[0]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -51,7 +47,7 @@ func (x *Empty) String() string {
 func (*Empty) ProtoMessage() {}
 
 func (x *Empty) ProtoReflect() protoreflect.Message {
-	mi := &file_iot_proto_msgTypes[0]
+	mi := &file_pkg_iot_iot_proto_msgTypes[0]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -64,7 +60,95 @@ func (x *Empty) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Empty.ProtoReflect.Descriptor instead.
 func (*Empty) Descriptor() ([]byte, []int) {
-	return file_iot_proto_rawDescGZIP(), []int{0}
+	return file_pkg_iot_iot_proto_rawDescGZIP(), []int{0}
+}
+
+// <discovery_prefix>/<component>/[<node_id>/]<object_id>/config
+type DeviceDiscovery struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	DiscoveryPrefix string   `protobuf:"bytes,1,opt,name=discovery_prefix,json=discoveryPrefix,proto3" json:"discovery_prefix,omitempty"`
+	Component       string   `protobuf:"bytes,2,opt,name=component,proto3" json:"component,omitempty"`
+	NodeId          string   `protobuf:"bytes,3,opt,name=node_id,json=nodeId,proto3" json:"node_id,omitempty"`
+	ObjectId        string   `protobuf:"bytes,4,opt,name=object_id,json=objectId,proto3" json:"object_id,omitempty"`
+	Endpoint        []string `protobuf:"bytes,5,rep,name=endpoint,proto3" json:"endpoint,omitempty"`
+	Message         []byte   `protobuf:"bytes,6,opt,name=message,proto3" json:"message,omitempty"`
+}
+
+func (x *DeviceDiscovery) Reset() {
+	*x = DeviceDiscovery{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_pkg_iot_iot_proto_msgTypes[1]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *DeviceDiscovery) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*DeviceDiscovery) ProtoMessage() {}
+
+func (x *DeviceDiscovery) ProtoReflect() protoreflect.Message {
+	mi := &file_pkg_iot_iot_proto_msgTypes[1]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use DeviceDiscovery.ProtoReflect.Descriptor instead.
+func (*DeviceDiscovery) Descriptor() ([]byte, []int) {
+	return file_pkg_iot_iot_proto_rawDescGZIP(), []int{1}
+}
+
+func (x *DeviceDiscovery) GetDiscoveryPrefix() string {
+	if x != nil {
+		return x.DiscoveryPrefix
+	}
+	return ""
+}
+
+func (x *DeviceDiscovery) GetComponent() string {
+	if x != nil {
+		return x.Component
+	}
+	return ""
+}
+
+func (x *DeviceDiscovery) GetNodeId() string {
+	if x != nil {
+		return x.NodeId
+	}
+	return ""
+}
+
+func (x *DeviceDiscovery) GetObjectId() string {
+	if x != nil {
+		return x.ObjectId
+	}
+	return ""
+}
+
+func (x *DeviceDiscovery) GetEndpoint() []string {
+	if x != nil {
+		return x.Endpoint
+	}
+	return nil
+}
+
+func (x *DeviceDiscovery) GetMessage() []byte {
+	if x != nil {
+		return x.Message
+	}
+	return nil
 }
 
 type Click struct {
@@ -80,7 +164,7 @@ type Click struct {
 func (x *Click) Reset() {
 	*x = Click{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_iot_proto_msgTypes[1]
+		mi := &file_pkg_iot_iot_proto_msgTypes[2]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -93,7 +177,7 @@ func (x *Click) String() string {
 func (*Click) ProtoMessage() {}
 
 func (x *Click) ProtoReflect() protoreflect.Message {
-	mi := &file_iot_proto_msgTypes[1]
+	mi := &file_pkg_iot_iot_proto_msgTypes[2]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -106,7 +190,7 @@ func (x *Click) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Click.ProtoReflect.Descriptor instead.
 func (*Click) Descriptor() ([]byte, []int) {
-	return file_iot_proto_rawDescGZIP(), []int{1}
+	return file_pkg_iot_iot_proto_rawDescGZIP(), []int{2}
 }
 
 func (x *Click) GetCount() string {
@@ -130,56 +214,67 @@ func (x *Click) GetZone() string {
 	return ""
 }
 
-var File_iot_proto protoreflect.FileDescriptor
+var File_pkg_iot_iot_proto protoreflect.FileDescriptor
 
-var file_iot_proto_rawDesc = []byte{
-	0x0a, 0x09, 0x69, 0x6f, 0x74, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x03, 0x69, 0x6f, 0x74,
-	0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d, 0x70, 0x74, 0x79, 0x22, 0x49, 0x0a, 0x05, 0x43, 0x6c, 0x69,
-	0x63, 0x6b, 0x12, 0x14, 0x0a, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28,
-	0x09, 0x52, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x64, 0x65, 0x76, 0x69,
-	0x63, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65,
-	0x12, 0x12, 0x0a, 0x04, 0x7a, 0x6f, 0x6e, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x04,
-	0x7a, 0x6f, 0x6e, 0x65, 0x32, 0x2b, 0x0a, 0x03, 0x49, 0x4f, 0x54, 0x12, 0x24, 0x0a, 0x0a, 0x43,
-	0x6c, 0x69, 0x63, 0x6b, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x12, 0x0a, 0x2e, 0x69, 0x6f, 0x74, 0x2e,
-	0x43, 0x6c, 0x69, 0x63, 0x6b, 0x1a, 0x0a, 0x2e, 0x69, 0x6f, 0x74, 0x2e, 0x45, 0x6d, 0x70, 0x74,
-	0x79, 0x42, 0x07, 0x5a, 0x05, 0x2e, 0x3b, 0x69, 0x6f, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74,
-	0x6f, 0x33,
+var file_pkg_iot_iot_proto_rawDesc = []byte{
+	0x0a, 0x11, 0x70, 0x6b, 0x67, 0x2f, 0x69, 0x6f, 0x74, 0x2f, 0x69, 0x6f, 0x74, 0x2e, 0x70, 0x72,
+	0x6f, 0x74, 0x6f, 0x12, 0x03, 0x69, 0x6f, 0x74, 0x22, 0x07, 0x0a, 0x05, 0x45, 0x6d, 0x70, 0x74,
+	0x79, 0x22, 0xc6, 0x01, 0x0a, 0x0f, 0x44, 0x65, 0x76, 0x69, 0x63, 0x65, 0x44, 0x69, 0x73, 0x63,
+	0x6f, 0x76, 0x65, 0x72, 0x79, 0x12, 0x29, 0x0a, 0x10, 0x64, 0x69, 0x73, 0x63, 0x6f, 0x76, 0x65,
+	0x72, 0x79, 0x5f, 0x70, 0x72, 0x65, 0x66, 0x69, 0x78, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x0f, 0x64, 0x69, 0x73, 0x63, 0x6f, 0x76, 0x65, 0x72, 0x79, 0x50, 0x72, 0x65, 0x66, 0x69, 0x78,
+	0x12, 0x1c, 0x0a, 0x09, 0x63, 0x6f, 0x6d, 0x70, 0x6f, 0x6e, 0x65, 0x6e, 0x74, 0x18, 0x02, 0x20,
+	0x01, 0x28, 0x09, 0x52, 0x09, 0x63, 0x6f, 0x6d, 0x70, 0x6f, 0x6e, 0x65, 0x6e, 0x74, 0x12, 0x17,
+	0x0a, 0x07, 0x6e, 0x6f, 0x64, 0x65, 0x5f, 0x69, 0x64, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x06, 0x6e, 0x6f, 0x64, 0x65, 0x49, 0x64, 0x12, 0x1b, 0x0a, 0x09, 0x6f, 0x62, 0x6a, 0x65, 0x63,
+	0x74, 0x5f, 0x69, 0x64, 0x18, 0x04, 0x20, 0x01, 0x28, 0x09, 0x52, 0x08, 0x6f, 0x62, 0x6a, 0x65,
+	0x63, 0x74, 0x49, 0x64, 0x12, 0x1a, 0x0a, 0x08, 0x65, 0x6e, 0x64, 0x70, 0x6f, 0x69, 0x6e, 0x74,
+	0x18, 0x05, 0x20, 0x03, 0x28, 0x09, 0x52, 0x08, 0x65, 0x6e, 0x64, 0x70, 0x6f, 0x69, 0x6e, 0x74,
+	0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x06, 0x20, 0x01, 0x28,
+	0x0c, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x49, 0x0a, 0x05, 0x43, 0x6c,
+	0x69, 0x63, 0x6b, 0x12, 0x14, 0x0a, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x18, 0x01, 0x20, 0x01,
+	0x28, 0x09, 0x52, 0x05, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x12, 0x16, 0x0a, 0x06, 0x64, 0x65, 0x76,
+	0x69, 0x63, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x64, 0x65, 0x76, 0x69, 0x63,
+	0x65, 0x12, 0x12, 0x0a, 0x04, 0x7a, 0x6f, 0x6e, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x04, 0x7a, 0x6f, 0x6e, 0x65, 0x42, 0x22, 0x5a, 0x20, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e,
+	0x63, 0x6f, 0x6d, 0x2f, 0x78, 0x61, 0x71, 0x75, 0x65, 0x32, 0x30, 0x38, 0x2f, 0x7a, 0x6e, 0x65,
+	0x74, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x69, 0x6f, 0x74, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f,
+	0x33,
 }
 
 var (
-	file_iot_proto_rawDescOnce sync.Once
-	file_iot_proto_rawDescData = file_iot_proto_rawDesc
+	file_pkg_iot_iot_proto_rawDescOnce sync.Once
+	file_pkg_iot_iot_proto_rawDescData = file_pkg_iot_iot_proto_rawDesc
 )
 
-func file_iot_proto_rawDescGZIP() []byte {
-	file_iot_proto_rawDescOnce.Do(func() {
-		file_iot_proto_rawDescData = protoimpl.X.CompressGZIP(file_iot_proto_rawDescData)
+func file_pkg_iot_iot_proto_rawDescGZIP() []byte {
+	file_pkg_iot_iot_proto_rawDescOnce.Do(func() {
+		file_pkg_iot_iot_proto_rawDescData = protoimpl.X.CompressGZIP(file_pkg_iot_iot_proto_rawDescData)
 	})
-	return file_iot_proto_rawDescData
+	return file_pkg_iot_iot_proto_rawDescData
 }
 
-var file_iot_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
-var file_iot_proto_goTypes = []interface{}{
-	(*Empty)(nil), // 0: iot.Empty
-	(*Click)(nil), // 1: iot.Click
+var file_pkg_iot_iot_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
+var file_pkg_iot_iot_proto_goTypes = []interface{}{
+	(*Empty)(nil),           // 0: iot.Empty
+	(*DeviceDiscovery)(nil), // 1: iot.DeviceDiscovery
+	(*Click)(nil),           // 2: iot.Click
 }
-var file_iot_proto_depIdxs = []int32{
-	1, // 0: iot.IOT.ClickEvent:input_type -> iot.Click
-	0, // 1: iot.IOT.ClickEvent:output_type -> iot.Empty
-	1, // [1:2] is the sub-list for method output_type
-	0, // [0:1] is the sub-list for method input_type
+var file_pkg_iot_iot_proto_depIdxs = []int32{
+	0, // [0:0] is the sub-list for method output_type
+	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
 	0, // [0:0] is the sub-list for extension extendee
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_iot_proto_init() }
-func file_iot_proto_init() {
-	if File_iot_proto != nil {
+func init() { file_pkg_iot_iot_proto_init() }
+func file_pkg_iot_iot_proto_init() {
+	if File_pkg_iot_iot_proto != nil {
 		return
 	}
 	if !protoimpl.UnsafeEnabled {
-		file_iot_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
+		file_pkg_iot_iot_proto_msgTypes[0].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Empty); i {
 			case 0:
 				return &v.state
@@ -191,7 +286,19 @@ func file_iot_proto_init() {
 				return nil
 			}
 		}
-		file_iot_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+		file_pkg_iot_iot_proto_msgTypes[1].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*DeviceDiscovery); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_pkg_iot_iot_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Click); i {
 			case 0:
 				return &v.state
@@ -208,98 +315,18 @@ func file_iot_proto_init() {
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_iot_proto_rawDesc,
+			RawDescriptor: file_pkg_iot_iot_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
-			NumServices:   1,
+			NumServices:   0,
 		},
-		GoTypes:           file_iot_proto_goTypes,
-		DependencyIndexes: file_iot_proto_depIdxs,
-		MessageInfos:      file_iot_proto_msgTypes,
+		GoTypes:           file_pkg_iot_iot_proto_goTypes,
+		DependencyIndexes: file_pkg_iot_iot_proto_depIdxs,
+		MessageInfos:      file_pkg_iot_iot_proto_msgTypes,
 	}.Build()
-	File_iot_proto = out.File
-	file_iot_proto_rawDesc = nil
-	file_iot_proto_goTypes = nil
-	file_iot_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// IOTClient is the client API for IOT service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type IOTClient interface {
-	ClickEvent(ctx context.Context, in *Click, opts ...grpc.CallOption) (*Empty, error)
-}
-
-type iOTClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewIOTClient(cc grpc.ClientConnInterface) IOTClient {
-	return &iOTClient{cc}
-}
-
-func (c *iOTClient) ClickEvent(ctx context.Context, in *Click, opts ...grpc.CallOption) (*Empty, error) {
-	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/iot.IOT/ClickEvent", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// IOTServer is the server API for IOT service.
-type IOTServer interface {
-	ClickEvent(context.Context, *Click) (*Empty, error)
-}
-
-// UnimplementedIOTServer can be embedded to have forward compatible implementations.
-type UnimplementedIOTServer struct {
-}
-
-func (*UnimplementedIOTServer) ClickEvent(context.Context, *Click) (*Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method ClickEvent not implemented")
-}
-
-func RegisterIOTServer(s *grpc.Server, srv IOTServer) {
-	s.RegisterService(&_IOT_serviceDesc, srv)
-}
-
-func _IOT_ClickEvent_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Click)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(IOTServer).ClickEvent(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/iot.IOT/ClickEvent",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(IOTServer).ClickEvent(ctx, req.(*Click))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _IOT_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "iot.IOT",
-	HandlerType: (*IOTServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "ClickEvent",
-			Handler:    _IOT_ClickEvent_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "iot.proto",
+	File_pkg_iot_iot_proto = out.File
+	file_pkg_iot_iot_proto_rawDesc = nil
+	file_pkg_iot_iot_proto_goTypes = nil
+	file_pkg_iot_iot_proto_depIdxs = nil
 }
