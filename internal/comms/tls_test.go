ed1df3b0ca7cc741c7041a07e029877399884c03
Zach Leslie <xaque208@gmail.com>
2021-02-02 22:15:00 -0800

    tests(comms): tidy up interface tests

--- internal/comms/tls_test.go
+++ internal/comms/tls_test.go
@@ -1,12 +1,15 @@
+// +build unit
+
 package comms
 
 import (
 	"testing"
 
 	"github.com/johanbrandhorst/certify"
+	"github.com/stretchr/testify/require"
 )
 
 func TestSingletonKey(t *testing.T) {
 	var x certify.KeyGenerator = &singletonKey{}
-	t.Logf("singletonKey: %+v", x)
+	require.NotNil(t, x)
 }
