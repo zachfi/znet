4e4149667d7466c8e682c7040aed63e939efe955
Zach Leslie <xaque208@gmail.com>
2021-02-05 19:05:28 -0800

    chore(network): lint network package

--- internal/network/network.go
+++ internal/network/network.go
@@ -1,10 +1,17 @@
 package network
 
 import (
+	"context"
+	"strings"
+	"sync"
+
 	"google.golang.org/grpc"
 
+	junos "github.com/scottdware/go-junos"
+	log "github.com/sirupsen/logrus"
 	"github.com/xaque208/znet/internal/config"
-	"github.com/xaque208/znet/pkg/events"
+	"github.com/xaque208/znet/internal/inventory"
+	"github.com/xaque208/znet/internal/telemetry"
 )
 
 // Agent is an RPC client worker bee.
@@ -21,19 +28,54 @@ func NewNetwork(cfg *config.NetworkConfig, conn *grpc.ClientConn) *Network {
 	}
 }
 
-func (n *Network) Subscriptions() *events.Subscriptions {
-	s := events.NewSubscriptions()
+// ScrapeJunosHost makes API calls against Junos calls to get some information.
+func (n *Network) ScrapeJunosHost(wg *sync.WaitGroup, h *inventory.NetworkHost) {
+	telemetryClient := telemetry.NewTelemetryClient(n.conn)
 
-	eventNames := []string{
-		"ARPUpdate",
+	hostName := strings.Join([]string{h.Name, h.Domain}, ".")
+	log.Debugf("scraping ARP status from host: %s", hostName)
+
+	auth := &junos.AuthMethod{
+		Username:   n.config.Junos.Username,
+		PrivateKey: n.config.Junos.PrivateKey,
 	}
 
-	for _, e := range eventNames {
-		switch e {
-		case "NamedTimer":
-			s.Subscribe(e, n.namedTimerHandler)
+	session, err := junos.NewSession(hostName, auth)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer session.Close()
+	defer wg.Done()
+
+	views, err := session.View("arp")
+	if err != nil {
+		log.Error(err)
+		wg.Done()
+		return
+	}
+
+	for _, arp := range views.Arp.Entries {
+		if arp.Interface == "ppp0.0" {
+			continue
 		}
-	}
 
-	return s
+		log.Tracef("reporting NetworkID: %+v", arp)
+
+		name := strings.ToLower(strings.Join([]string{arp.MACAddress, arp.Interface}, "_"))
+
+		networkID := &inventory.NetworkID{
+			Name:                     name,
+			IpAddress:                []string{arp.IPAddress},
+			MacAddress:               []string{arp.MACAddress},
+			ReportingSource:          []string{h.Name},
+			ReportingSourceInterface: []string{arp.Interface},
+		}
+
+		_, err = telemetryClient.ReportNetworkID(context.Background(), networkID)
+		if err != nil {
+			log.Error(err)
+		}
+
+	}
 }
