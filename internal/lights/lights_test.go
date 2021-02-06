9af96b6cbd46487300bbdb2be87537aaaf2f39a4
Zach Leslie <xaque208@gmail.com>
2021-02-06 12:18:56 -0800

    tests(lights): add structure to tests

--- internal/lights/lights_test.go
+++ internal/lights/lights_test.go
@@ -18,50 +18,60 @@ func TestNewLights(t *testing.T) {
 }
 
 func TestAlert(t *testing.T) {
-	h := &mockLight{}
-	l := &Lights{
-		config:   &config.LightsConfig{},
-		handlers: []Handler{h},
+
+	testCases := []struct {
+		Handler *MockLight
+	}{}
+
+	for _, tc := range testCases {
+		h := &MockLight{}
+		l := &Lights{
+			config:   &config.LightsConfig{},
+			handlers: []Handler{h},
+		}
+
+		groupName := &LightGroupRequest{
+			Name: "dungeon",
+		}
+		ctx := context.Background()
+
+		_, err := l.On(ctx, groupName)
+		require.NoError(t, err)
+		// TODO
+		require.Equal(t, tc.Handler, h)
+
+		_, err = l.Off(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.OffCalls)
+
+		_, err = l.Alert(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.AlertCalls)
+
+		_, err = l.Dim(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.DimCalls)
+
+		_, err = l.SetColor(ctx, groupName)
+		require.Error(t, err)
+		require.Equal(t, 0, h.SetColorCalls)
+		groupName.Color = "#ffffff"
+		_, err = l.SetColor(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.SetColorCalls)
+
+		_, err = l.Toggle(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.ToggleCalls)
+
+		_, err = l.RandomColor(ctx, groupName)
+		require.Error(t, err)
+		require.Equal(t, 0, h.RandomColorCalls)
+		groupName.Colors = []string{"#ffffff"}
+		_, err = l.RandomColor(ctx, groupName)
+		require.NoError(t, err)
+		require.Equal(t, 1, h.SetColorCalls)
+
 	}
 
-	groupName := &LightGroupRequest{
-		Name: "dungeon",
-	}
-	ctx := context.Background()
-
-	_, err := l.On(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.onCalls)
-
-	_, err = l.Off(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.offCalls)
-
-	_, err = l.Alert(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.alertCalls)
-
-	_, err = l.Dim(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.dimCalls)
-
-	_, err = l.SetColor(ctx, groupName)
-	require.Error(t, err)
-	require.Equal(t, 0, h.setColorCalls)
-	groupName.Color = "#ffffff"
-	_, err = l.SetColor(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.setColorCalls)
-
-	_, err = l.Toggle(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.toggleCalls)
-
-	_, err = l.RandomColor(ctx, groupName)
-	require.Error(t, err)
-	require.Equal(t, 0, h.randomColorCalls)
-	groupName.Colors = []string{"#ffffff"}
-	_, err = l.RandomColor(ctx, groupName)
-	require.NoError(t, err)
-	require.Equal(t, 1, h.setColorCalls)
 }
