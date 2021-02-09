99e9e7c36b5cf684d9e32595a1f5a27c20f3830d
Zach Leslie <xaque208@gmail.com>
2021-02-08 21:20:49 -0800

    chore(timer): update protobufs

--- internal/timer/timer.pb.go
+++ internal/timer/timer.pb.go
@@ -7,17 +7,12 @@
 package timer
 
 import (
-	context "context"
-	reflect "reflect"
-	sync "sync"
-
 	proto "github.com/golang/protobuf/proto"
 	timestamp "github.com/golang/protobuf/ptypes/timestamp"
-	grpc "google.golang.org/grpc"
-	codes "google.golang.org/grpc/codes"
-	status "google.golang.org/grpc/status"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
+	reflect "reflect"
+	sync "sync"
 )
 
 const (
@@ -223,84 +218,4 @@ func file_internal_timer_timer_proto_init() {
 	file_internal_timer_timer_proto_rawDesc = nil
 	file_internal_timer_timer_proto_goTypes = nil
 	file_internal_timer_timer_proto_depIdxs = nil
-}
-
-// Reference imports to suppress errors if they are not otherwise used.
-var _ context.Context
-var _ grpc.ClientConnInterface
-
-// This is a compile-time assertion to ensure that this generated file
-// is compatible with the grpc package it is being compiled against.
-const _ = grpc.SupportPackageIsVersion6
-
-// TimerClient is the client API for Timer service.
-//
-// For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
-type TimerClient interface {
-	NamedTimer(ctx context.Context, in *NamedTimeStamp, opts ...grpc.CallOption) (*Empty, error)
-}
-
-type timerClient struct {
-	cc grpc.ClientConnInterface
-}
-
-func NewTimerClient(cc grpc.ClientConnInterface) TimerClient {
-	return &timerClient{cc}
-}
-
-func (c *timerClient) NamedTimer(ctx context.Context, in *NamedTimeStamp, opts ...grpc.CallOption) (*Empty, error) {
-	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/timer.Timer/NamedTimer", in, out, opts...)
-	if err != nil {
-		return nil, err
-	}
-	return out, nil
-}
-
-// TimerServer is the server API for Timer service.
-type TimerServer interface {
-	NamedTimer(context.Context, *NamedTimeStamp) (*Empty, error)
-}
-
-// UnimplementedTimerServer can be embedded to have forward compatible implementations.
-type UnimplementedTimerServer struct {
-}
-
-func (*UnimplementedTimerServer) NamedTimer(context.Context, *NamedTimeStamp) (*Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method NamedTimer not implemented")
-}
-
-func RegisterTimerServer(s *grpc.Server, srv TimerServer) {
-	s.RegisterService(&_Timer_serviceDesc, srv)
-}
-
-func _Timer_NamedTimer_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(NamedTimeStamp)
-	if err := dec(in); err != nil {
-		return nil, err
-	}
-	if interceptor == nil {
-		return srv.(TimerServer).NamedTimer(ctx, in)
-	}
-	info := &grpc.UnaryServerInfo{
-		Server:     srv,
-		FullMethod: "/timer.Timer/NamedTimer",
-	}
-	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(TimerServer).NamedTimer(ctx, req.(*NamedTimeStamp))
-	}
-	return interceptor(ctx, in, info, handler)
-}
-
-var _Timer_serviceDesc = grpc.ServiceDesc{
-	ServiceName: "timer.Timer",
-	HandlerType: (*TimerServer)(nil),
-	Methods: []grpc.MethodDesc{
-		{
-			MethodName: "NamedTimer",
-			Handler:    _Timer_NamedTimer_Handler,
-		},
-	},
-	Streams:  []grpc.StreamDesc{},
-	Metadata: "internal/timer/timer.proto",
 }
