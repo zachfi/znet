3267c3d7fd792295ccbd0686b28c4abb682b9320
Zach Leslie <xaque208@gmail.com>
2021-10-17 19:42:10 -0600

    chore(znet): further adopt pattern from grafana Tempo

--- znet/modules.go
+++ znet/modules.go
@@ -1,29 +1,94 @@
 package znet
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/go-kit/log/level"
+	"github.com/grafana/dskit/modules"
 	"github.com/grafana/dskit/services"
+	"github.com/pkg/errors"
+	"github.com/weaveworks/common/server"
+	"github.com/xaque208/znet/internal/astro"
+	"github.com/xaque208/znet/internal/comms"
+	"github.com/xaque208/znet/internal/timer"
+	"github.com/xaque208/znet/internal/timer/named"
 	"github.com/xaque208/znet/modules/harvester"
-	"github.com/xaque208/znet/modules/server"
 )
 
 type module int
 
 const (
-	Harvester module = iota
-	// Timer
-	// Agent
-	Server
+	Server string = "server"
+
+	// Harvester string = "harvester"
+	Timer string = "timer"
+
+	// Telemetry string = "telemetry"
+
+	// TODO currently we are using openweathermap_exporter as a source of data
+	// for astro data when sending events to the server.  Perhaps it makes more
+	// sense to include the exporter as a module, and then we can either
+	// reference internal values, or provide RPC modules for requesting the
+	// information necessary.  Without this, there is a requried coordination
+	// between the exporter (another project), and the znet.
+	// Weather string = "telemetry"
+
+	All string = "all"
 )
 
-func (m module) String() string {
-	return [...]string{
-		"Harvester",
-		"Timer",
-		"Agent",
-		"Server",
-	}[m]
+func (z *Znet) setupModuleManager() error {
+	mm := modules.NewManager(z.logger)
+	mm.RegisterModule(Server, z.initServer, modules.UserInvisibleModule)
+	// mm.RegisterModule(Harvester, z.initHarvester)
+	mm.RegisterModule(Timer, z.initTimer)
+	mm.RegisterModule(All, nil)
+
+	deps := map[string][]string{
+		// Server:       nil,
+
+		// Timer: {Server, Lights},
+		// IOT: {Server},
+		// Inventory: {Server},
+		// Lights: {Server},
+		// Astro: {Server},
+		// Telemetry: {Server, IOT, Lights},
+
+		// TODO
+		// gitwatch:       nil,
+		// build:       nil,
+		// agent:       nil,
+
+		// Harvester: {Server},
+		Timer: {Server},
+		All:   {Server, Timer},
+	}
+
+	for mod, targets := range deps {
+		if err := mm.AddDependency(mod, targets...); err != nil {
+			return err
+		}
+	}
+
+	z.ModuleManager = mm
+
+	return nil
+}
+
+func (z *Znet) initTimer() (services.Service, error) {
+
+	conn := comms.SlimRPCClient(z.cfg.RPC.ServerAddress)
+
+	t, err := timer.New(z.cfg.Timer, z.logger, conn)
+	if err != nil {
+		return nil, errors.Wrap(err, "unable to init timer")
+	}
+
+	astro.RegisterAstroServer(z.Server.GRPC, t.Astro)
+	named.RegisterNamedServer(z.Server.GRPC, t.Named)
+
+	z.timer = t
+	return t, nil
 }
 
 func (z *Znet) initHarvester() (services.Service, error) {
@@ -33,15 +98,66 @@ func (z *Znet) initHarvester() (services.Service, error) {
 	}
 
 	z.harvester = h
-	return z.harvester, nil
+	return h, nil
 }
 
 func (z *Znet) initServer() (services.Service, error) {
-	s, err := server.New(z.cfg.Server)
+	z.cfg.Server.MetricsNamespace = metricsNamespace
+	z.cfg.Server.ExcludeRequestInLog = true
+
+	// cortex.DisableSignalHandling(&t.cfg.Server)
+
+	server, err := server.New(z.cfg.Server)
 	if err != nil {
-		return nil, fmt.Errorf("failed to create harvester")
+		return nil, errors.Wrap(err, "failed to create server")
 	}
 
-	z.server = s
-	return z.server, nil
+	servicesToWaitFor := func() []services.Service {
+		svs := []services.Service(nil)
+		for m, s := range z.serviceMap {
+			// Server should not wait for itself.
+			if m != Server {
+				svs = append(svs, s)
+			}
+		}
+		return svs
+	}
+
+	z.Server = server
+
+	serverDone := make(chan error, 1)
+
+	runFn := func(ctx context.Context) error {
+		go func() {
+			defer close(serverDone)
+			serverDone <- server.Run()
+		}()
+
+		select {
+		case <-ctx.Done():
+			return nil
+		case err := <-serverDone:
+			if err != nil {
+				return err
+			}
+			return fmt.Errorf("server stopped unexpectedly")
+		}
+	}
+
+	stoppingFn := func(_ error) error {
+		// wait until all modules are done, and then shutdown server.
+		for _, s := range servicesToWaitFor() {
+			_ = s.AwaitTerminated(context.Background())
+		}
+
+		// shutdown HTTP and gRPC servers (this also unblocks Run)
+		server.Shutdown()
+
+		// if not closed yet, wait until server stops.
+		<-serverDone
+		level.Info(z.logger).Log("msg", "server stopped")
+		return nil
+	}
+
+	return services.NewBasicService(nil, runFn, stoppingFn), nil
 }
--- znet/znet.go
+++ znet/znet.go
@@ -1,15 +1,20 @@
 package znet
 
 import (
+	"context"
 	"fmt"
 
 	"github.com/go-kit/log"
 	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/modules"
+	"github.com/grafana/dskit/services"
+	"github.com/pkg/errors"
+	"github.com/weaveworks/common/server"
+	"github.com/weaveworks/common/signals"
 
+	"github.com/xaque208/znet/internal/timer"
 	"github.com/xaque208/znet/modules/harvester"
-	"github.com/xaque208/znet/modules/server"
-	"github.com/xaque208/znet/pkg/netconfig"
+	"github.com/xaque208/znet/pkg/util"
 )
 
 const metricsNamespace = "znet"
@@ -17,54 +22,130 @@ const metricsNamespace = "znet"
 // Znet is the core object for this project.  It keeps track of the data,
 // configuration and flow control for starting the server process.
 type Znet struct {
-	cfg *Config
+	cfg Config
 
-	ConfigDir   string
-	Data        netconfig.Data
-	Environment map[string]string
+	// ConfigDir   string
+	// Data        netconfig.Data
+	// Environment map[string]string
+
+	Server *server.Server
 
 	logger log.Logger
 
+	// Modules.
 	harvester *harvester.Harvester
 	server    *server.Server
+	timer     *timer.Timer
+
+	ModuleManager *modules.Manager
+	serviceMap    map[string]services.Service
 }
 
-// NewZnet creates and returns a new Znet object.
-func NewZnet(file string, l log.Logger) (*Znet, error) {
-	var err error
-	var environment map[string]string
+// New creates and returns a new Znet object.
+func New(cfg Config) (*Znet, error) {
+	z := &Znet{
+		cfg: cfg,
+	}
 
-	l.Log("msg", "loading config", "file", file)
+	z.logger = util.NewLogger()
 
-	cfg, err := loadConfig(file)
+	if z.cfg.Target == "" {
+		z.cfg.Target = All
+	}
+
+	// var err error
+	// var environment map[string]string
+	//
+	// l.Log("msg", "loading config", "file", file)
+	//
+	// cfg, err := loadConfig(file)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to load config file %s: %w", file, err)
+	// }
+	//
+	// if cfg.Environments != nil && cfg.Vault != nil {
+	// 	e, err := getEnvironmentConfig(*cfg.Environments, "common")
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("failed to get environment config: %w", err)
+	// 	}
+	//
+	// 	environment, err = LoadEnvironment(cfg.Vault, e)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("failed to load environment: %w", err)
+	// 	}
+	// } else {
+	// 	level.Debug(l).Log("missing vault/environment config")
+	// }
+	//
+	// z := Znet{
+	// 	cfg:         cfg,
+	// 	Environment: environment,
+	// }
+
+	if err := z.setupModuleManager(); err != nil {
+		return nil, errors.Wrap(err, "failed to setup module manager")
+	}
+
+	return z, nil
+}
+
+func (z *Znet) Run() error {
+	serviceMap, err := z.ModuleManager.InitModuleServices(z.cfg.Target)
 	if err != nil {
-		return nil, fmt.Errorf("failed to load config file %s: %w", file, err)
+		return fmt.Errorf("failed to init module services %w", err)
+	}
+	z.serviceMap = serviceMap
+
+	servs := []services.Service(nil)
+	for _, s := range serviceMap {
+		servs = append(servs, s)
 	}
 
-	if cfg.Environments != nil && cfg.Vault != nil {
-		e, err := getEnvironmentConfig(*cfg.Environments, "common")
-		if err != nil {
-			return nil, fmt.Errorf("failed to get environment config: %w", err)
+	sm, err := services.NewManager(servs...)
+	if err != nil {
+		return fmt.Errorf("failed to start service manager %w", err)
+	}
+
+	// TODO setup http handlers
+	// z.Server.HTTP.Path("/ready").Handler(z.readyHandler(sm))
+	// grpc_health_v1.RegisterHealthServer(z.Server.GRPC, healthcheck.New(sm))
+
+	// Listen for events from this manager, and log them.
+	healthy := func() { level.Info(z.logger).Log("msg", "zNet started") }
+	stopped := func() { level.Info(z.logger).Log("msg", "zNet stopped") }
+	serviceFailed := func(service services.Service) {
+		// if any service fails, stop everything
+		sm.StopAsync()
+
+		// let's find out which module failed
+		for m, s := range serviceMap {
+			if s == service {
+				if service.FailureCase() == modules.ErrStopProcess {
+					level.Info(z.logger).Log("msg", "received stop signal via return error", "module", m, "err", service.FailureCase())
+				} else {
+					level.Error(z.logger).Log("msg", "module failed", "module", m, "err", service.FailureCase())
+				}
+				return
+			}
 		}
 
-		environment, err = LoadEnvironment(cfg.Vault, e)
-		if err != nil {
-			return nil, fmt.Errorf("failed to load environment: %w", err)
-		}
-	} else {
-		level.Debug(l).Log("missing vault/environment config")
+		level.Error(z.logger).Log("msg", "module failed", "module", "unknown", "err", service.FailureCase())
+	}
+	sm.AddListener(services.NewManagerListener(healthy, stopped, serviceFailed))
+
+	// Setup signal handler. If signal arrives, we stop the manager, which stops all the services.
+	handler := signals.NewHandler(z.Server.Log)
+	go func() {
+		handler.Loop()
+		sm.StopAsync()
+	}()
+
+	// Start all services. This can really only fail if some service is already
+	// in other state than New, which should not be the case.
+	err = sm.StartAsync(context.Background())
+	if err != nil {
+		return fmt.Errorf("failed to start service manager %w", err)
 	}
 
-	z := Znet{
-		cfg:         cfg,
-		Environment: environment,
-	}
-
-	return &z, nil
-}
-
-func (z *Znet) Run() {
-	mm := modules.NewManager(z.logger)
-	mm.RegisterModule(Harvester.String(), z.initHarvester)
-	mm.RegisterModule(Server.String(), z.initServer)
+	return sm.AwaitStopped(context.Background())
 }
