62ff34381caaae5ed3f952a538aabeb8c3094b6d
Zach Leslie <xaque208@gmail.com>
2021-10-31 20:44:37 -0600

    chore(harvester): implement service interface

--- modules/harvester/config.go
+++ modules/harvester/config.go
@@ -1,4 +1,5 @@
 package harvester
 
-type Config struct {
-}
+import "github.com/xaque208/znet/pkg/iot"
+
+type Config iot.Config
--- modules/harvester/harvester.go
+++ modules/harvester/harvester.go
@@ -3,20 +3,32 @@ package harvester
 import (
 	"context"
 
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/go-kit/log"
+	"github.com/go-kit/log/level"
 	"github.com/grafana/dskit/services"
-	"github.com/xaque208/znet/internal/telemetry"
+	"github.com/pkg/errors"
+	"github.com/xaque208/znet/modules/inventory"
+	"github.com/xaque208/znet/modules/telemetry"
+	"github.com/xaque208/znet/pkg/iot"
+	"google.golang.org/grpc"
 )
 
 type Harvester struct {
 	services.Service
+	cfg *Config
 
-	cfg             *Config
+	logger log.Logger
+
+	conn            *grpc.ClientConn
 	telemetryClient telemetry.TelemetryClient
 }
 
-func New(cfg Config) (*Harvester, error) {
+func New(cfg Config, logger log.Logger, conn *grpc.ClientConn) (*Harvester, error) {
 	h := &Harvester{
-		cfg: &cfg,
+		cfg:    &cfg,
+		logger: log.With(logger, "module", "harvester"),
+		conn:   conn,
 	}
 
 	h.Service = services.NewBasicService(h.starting, h.running, h.stopping)
@@ -25,22 +37,81 @@ func New(cfg Config) (*Harvester, error) {
 }
 
 func (h *Harvester) starting(ctx context.Context) error {
-	// conn := comms.StandardRPCClient(z.Config.RPC.ServerAddress, *cfg)
-	//
-	// defer func() {
-	// 	err = conn.Close()
-	// 	if err != nil {
-	// 		log.Error(err)
-	// 	}
-	// }()
-	//
-	// telemetryClient := telemetry.NewTelemetryClient(conn)
-	// h.telemetryClient = telemetryClient
+	telemetryClient := telemetry.NewTelemetryClient(h.conn)
+	h.telemetryClient = telemetryClient
 
 	return nil
 }
 
 func (h *Harvester) running(ctx context.Context) error {
+
+	var onMessageReceived mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+		topicPath, err := iot.ParseTopicPath(msg.Topic())
+		if err != nil {
+			level.Error(h.logger).Log("err", errors.Wrap(err, "failed to parse topic path"))
+			return
+		}
+
+		discovery := &iot.DeviceDiscovery{
+			Component: topicPath.Component,
+			NodeId:    topicPath.NodeID,
+			ObjectId:  topicPath.ObjectID,
+			Endpoint:  topicPath.Endpoint,
+			Message:   msg.Payload(),
+		}
+
+		iotDevice := &inventory.IOTDevice{
+			DeviceDiscovery: discovery,
+		}
+
+		_, err = h.telemetryClient.ReportIOTDevice(context.Background(), iotDevice)
+		if err != nil {
+			level.Error(h.logger).Log("err", err.Error())
+		}
+	}
+
+	mqttOpts := mqtt.NewClientOptions()
+	mqttOpts.AddBroker(h.cfg.MQTT.URL)
+	mqttOpts.SetCleanSession(true)
+	mqttOpts.OnConnect = func(c mqtt.Client) {
+		token := c.Subscribe(h.cfg.MQTT.Topic, 0, onMessageReceived)
+		token.Wait()
+		if token.Error() != nil {
+			level.Error(h.logger).Log("err", token.Error())
+		}
+	}
+
+	if h.cfg.MQTT.Username != "" && h.cfg.MQTT.Password != "" {
+		mqttOpts.Username = h.cfg.MQTT.Username
+		mqttOpts.Password = h.cfg.MQTT.Password
+	}
+
+	mqttClient := mqtt.NewClient(mqttOpts)
+
+	if token := mqttClient.Connect(); token.Wait() && token.Error() != nil {
+		level.Error(h.logger).Log("err", token.Error())
+	} else {
+		level.Debug(h.logger).Log("msg", "mqtt connected", "url", h.cfg.MQTT.URL)
+	}
+
+	// 	log.WithFields(log.Fields{
+	// 		"http": listenAddr,
+	// 	}).Debug("listening")
+	//
+	// 	go func() {
+	// 		sig := <-sigs
+	// 		log.Warnf("caught signal: %s", sig.String())
+	//
+	// 		done <- true
+	// 	}()
+	//
+	// 	<-done
+	//
+	// 	if token := mqttClient.Unsubscribe(mqttTopic); token.Wait() && token.Error() != nil {
+	// 		log.Error(token.Error())
+	// 	}
+	//
+	// 	mqttClient.Disconnect(250)
 
 	<-ctx.Done()
 
@@ -48,5 +119,5 @@ func (h *Harvester) running(ctx context.Context) error {
 }
 
 func (h *Harvester) stopping(_ error) error {
-	return nil
+	return h.conn.Close()
 }
