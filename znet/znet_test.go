9e1fb09108319e539bbc74c80ceb5027ad0abc0f
Zach Leslie <xaque208@gmail.com>
2021-11-03 20:03:50 -0600

    chore(docker): update Dockerfile

--- znet/znet_test.go
+++ znet/znet_test.go
@@ -9,11 +9,6 @@ import (
 )
 
 func TestNewZnet(t *testing.T) {
-	cfgFileNoExists := "../testdata/doesnotexist.yaml"
-	z, err := NewZnet(cfgFileNoExists)
-	require.Error(t, err, "failed to load config")
-	require.Nil(t, z)
-
 	cfgFile := "../testdata/config.yaml"
 	z, err = NewZnet(cfgFile)
 	require.NoError(t, err)
