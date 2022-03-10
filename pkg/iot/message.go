6c781e6f18295d487df30fc026b0053ec93dad56
Zach Leslie <xaque208@gmail.com>
2022-03-10 10:43:25 -0700

    Use float32 for temperature

--- pkg/iot/message.go
+++ pkg/iot/message.go
@@ -5,15 +5,15 @@ import (
 )
 
 type ZigbeeMessage struct {
-	Action      *string `json:"action,omitempty"`
-	Battery     *int    `json:"battery,omitempty"`
-	Illuminance *int    `json:"illuminance,omitempty"`
-	LinkQuality *int    `json:"linkquality,omitempty"`
-	Occupancy   *bool   `json:"occupancy,omitempty"`
-	Tamper      *bool   `json:"tamper,omitempty"`
-	Temperature *int    `json:"temperature,omitempty"`
-	Voltage     *int    `json:"voltage,omitempty"`
-	WaterLeak   *bool   `json:"water_leak,omitempty"`
+	Action      *string  `json:"action,omitempty"`
+	Battery     *int     `json:"battery,omitempty"`
+	Illuminance *int     `json:"illuminance,omitempty"`
+	LinkQuality *int     `json:"linkquality,omitempty"`
+	Occupancy   *bool    `json:"occupancy,omitempty"`
+	Tamper      *bool    `json:"tamper,omitempty"`
+	Temperature *float32 `json:"temperature,omitempty"`
+	Voltage     *int     `json:"voltage,omitempty"`
+	WaterLeak   *bool    `json:"water_leak,omitempty"`
 }
 
 type ZigbeeBridgeState string
